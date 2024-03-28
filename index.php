d105fe7be5c8c2ed54028a90c37aa21c7bceeda8
Harry Clark <139020900+astonHC@users.noreply.github.com>
2024-03-28 17:45:44 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
+?>
 <!DOCTYPE html>
 <html lang="en>
-
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -21,5 +21,3 @@
 	</main>
 </body>
 </html>
-
-?>
