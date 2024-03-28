0c41d18ce9b40ab7f9911583159530ac902abf8b
Harry Clark <139020900+astonHC@users.noreply.github.com>
2024-03-28 17:37:05 +0000

    initial commits

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en>
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/styles.css">
+	<title>Harry Clark - CS1_IAD Port 3</title>
+</head>
+<body>
+	<h1>Harry Clark - IAD Port 3</h1>
+	<main>
+		<form action="formhandler.php" method="post">
+		<label for="firstname">Firsname</label>
+		<input id="firstname" type="text" name="firstname">
+		<button type="submit">Submit</button>
+	</form>
+	</main>
+</body>
+</html>
