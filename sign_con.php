a48cafc1eace257c897decaa7aa44ac4eb205f60
Harry Clark <139020900+astonHC@users.noreply.github.com>
2024-04-04 19:18:51 +0000

    Update sign_con.php

--- sign_con.php
+++ sign_con.php
@@ -12,14 +12,11 @@
 // FOCUSSING ON THE CONTROLLER ASPECT WHICH WILL ACT AS THE MASTER LOGIC 
 // FOR THE REST OF THE SIGNUP PAGE
 
-class SignUpController
+class SignUpController extends SignUpPage
 {
     private static $uid;
     private static $email;
     private static $email_retype;
-
-    // CONSTRUCT THE CONTROLLER TYPE TO DISCERN BETWEEN THE VARIOUS INPUT METHODS
-    // THIS IS JUST TO INSTANTIATE THEM
 
     public function __construct($fields)
     {
@@ -27,49 +24,69 @@ class SignUpController
         $this->email = isset($fields['email']) ? $fields['email'] : null;
         $this->email_retype = isset($fields['email_retype']) ? $fields['email_retype'] : null;
     }
-    
-    // TENARY OPERTOR THAT RETURNS THE TRUE OR FALSE OUTPUT OF WHETHER
-    // THE CORRESPONDENCE FOR THE INPUT FIELD HAVE BEEN MET 
 
     private function EMPTY_INPUT()
     {
-        $RESULT = (empty($this->$uid) || empty($this->$email) || empty($this->$email_retype)) ? true : false;
-        return $RESULT;
+        return empty($this->uid) || empty($this->email) || empty($this->email_retype);
     }
 
     private function INVALID_INPUT($INPUT)
     {
-        // CREATE A MAP FOR THE VARIABLE DIRECTLY
-        // THINK OF IT AS THOUGH WE ARE MAPPING CHARS TO THE VARIABLES
-        // TO BE ABLE TO DISCEERN THROUGH DIFFERENT TYPES
-
-        $INPUT_TYPES = 
-        [
+        $INPUT_TYPES = [
             'uid' => $this->uid,
             'email' => $this->email,
             'email_retype' => $this->email_retype
         ];
 
-        switch($INPUT)
-        {
+        switch ($INPUT) {
             case 'uid':
-                return !preg_match("/^[a-zA-Z0-9]*$/", $this->uid) ? false : true;
-            break;
-
+                return !preg_match("/^[a-zA-Z0-9]*$/", $this->uid);
             case 'email':
-                return !preg_match("/^[a-zA-Z0-9]*$/", $this->email) ? false : true;
-            break;
-
+                return !filter_var($this->email, FILTER_VALIDATE_EMAIL);
             case 'email_retype':
-                return !preg_match("/^[a-zA-Z0-9]*$/", $this->email_retype) ? false : true;
-            break;
-
+                return !filter_var($this->email_retype, FILTER_VALIDATE_EMAIL);
             default:
                 throw new Exception("Invalid Variable Type");
         }
     }
 
-    
+    public function SIGNUP_USER()
+    {
+        if ($this->EMPTY_INPUT()) 
+        {
+            header("location: ../index.php?error=emptyinput");
+            exit();
+        }
+
+        if ($this->INVALID_INPUT('email')) 
+        {
+            header("location: ../index.php?error=invalid_email");
+            exit();
+        }
+
+        if ($this->email !== $this->email_retype) 
+        {
+            header("location: ../index.php?error=emails_dont_match");
+            exit();
+        }
+
+        if ($this->CHECK_USER($this->uid, $this->email)) 
+        {
+            header("location: ../index.php?error=user_exists");
+            exit();
+        }
+
+        $hashed_password = $this->HASHED_PWD($this->password);
+
+        if (!$this->CREATE_USER(["uid" => $this->uid, "email" => $this->email, "password" => $hashed_password])) 
+        {
+            header("location: ../index.php?error=signup_failed");
+            exit();
+        }
+
+        header("location: ../sign.php?success=signup");
+        exit();
+    }
 }
 
 ?>
