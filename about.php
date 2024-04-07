674653f3beb504ef52f99303dd74af629207a4fb
Harry Clark <139020900+astonHC@users.noreply.github.com>
2024-04-07 18:49:08 +0000

    Add files via upload

---
+++ about.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!----STYLE SHEET----->
+
+    <link rel="stylesheet" href="css/about.css">
+
+    <!----NOT SURE IF THIS IS THE CORRECT WAY TO IMPORT FONTS----->
+    <!----USING ARCH FOR THIS PORTFOLIO----->
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+    </style>
+
+    <title>Harry Clark - CS1_IAD Port 3</title>
+</head>
+<body>
+
+    <!----BASE FUNCTIONALITY FOR THE NAVBAR----->
+
+	<main-header>
+        <h1>Harry Clark - IAD Port 3</h1>
+        <nav>
+            <ul class="NAVBAR">
+                <li><a href="index.php">HOME</a></li>
+                <li><a href="about.php">ABOUT US</a></li>
+                <li><a href="login.php">LOG IN</a></li>
+            </ul>
+        </nav>
+	</main-header>
+
+    <section class="ABOUT-SECTION" id="ABOUT-SECTION">
+        <div class="ABOUT-IMG">
+            <img src="imgg/me.png">
+        </div>
+
+        <div class="ABOUT-TEXT">
+            <h2>--- About <span>AProject ---</span></h2>
+            <h4></h4>
+            <p>
+
+
+
+
+            </p>
+        </div>
+    </section>
+
+</body>
+</html>
