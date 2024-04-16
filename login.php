7a35cc4548dad094329bec17c10e80bbb539c9c4
Harry Clark <139020900+astonHC@users.noreply.github.com>
2024-04-16 12:21:36 +0000

    Update login.php

--- login.php
+++ login.php
@@ -80,6 +80,10 @@ if (isset($_POST['submitted']))
 <head>
     <title>Login Page</title>
     <link rel="stylesheet" href="css/login.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+    </style>
+
 </head>
 <body>
     <h2>Login</h2>
